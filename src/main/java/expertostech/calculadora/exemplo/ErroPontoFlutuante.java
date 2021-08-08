5881382466f754b2b14701d85b1687d3b3a22b2f
Rodrigo Tavares <tavares@netplataforma.com.br>
2021-08-08 20:25:35 -0300

    Como usar BigDecimal com Java ao invés de Double e Float

--- src/main/java/expertostech/calculadora/exemplo/ErroPontoFlutuante.java
+++ src/main/java/expertostech/calculadora/exemplo/ErroPontoFlutuante.java
@@ -3,7 +3,7 @@ package expertostech.calculadora.exemplo;
 public class ErroPontoFlutuante {
 
     public static void main(String[] args) {
-        double valor = 0.25;
+        double valor = 0.2;
         double total = 0.0;
 
         for (int i = 0; i < 10; i++) {
